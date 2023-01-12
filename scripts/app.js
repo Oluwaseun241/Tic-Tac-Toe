9aab509ddc80fa1aa343ac408c90bad7d5b9754b
Oluwaseun241 <tanimolaoluwaseun70@gmail.com>
2023-01-12 16:20:53 +0100

    overlay config

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,10 @@ const backdropElement = document.getElementById('backdrop');
 
 const editPlayer1Element = document.getElementById('edit-player1');
 const editPlayer2Element = document.getElementById('edit-player2');
+const cancelConfigElement = document.getElementById('cancel-config');
 
 editPlayer1Element.addEventListener('click', openPlayerConfig);
 editPlayer2Element.addEventListener('click', openPlayerConfig);
+
+cancelConfigElement.addEventListener('click', closePlayerConfig);
+backdropElement.addEventListener('click', closePlayerConfig);
--- scripts/config.js
+++ scripts/config.js
@@ -2,3 +2,8 @@ function openPlayerConfig() {
 	playerOverlayElement.style.display = 'block';
 	backdropElement.style.display = 'block';
 }
+
+function closePlayerConfig() {
+	playerOverlayElement.style.display = 'none';
+	backdropElement.style.display = 'none';
+}
