a842d59d4d382b9bd44a47e475957187a1b9e718
Oluwaseun <tanimolaoluwaseun70@gmail.com>
2023-01-13 22:38:23 +0100

    start game

--- scripts/app.js
+++ scripts/app.js
@@ -15,10 +15,13 @@ const playerOverlayElement = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
 const formElement = document.querySelector('form');
 const errorsElement = document.getElementById('config-errors');
+const startGameErrorElement = document.getElementById('start-game-error');
+const gameAreaElement = document.getElementById('active-game');
 
 const editPlayer1Element = document.getElementById('edit-player1');
 const editPlayer2Element = document.getElementById('edit-player2');
 const cancelConfigElement = document.getElementById('cancel-config');
+const startNewGameElement = document.getElementById('start-game-btn');
 
 editPlayer1Element.addEventListener('click', openPlayerConfig);
 editPlayer2Element.addEventListener('click', openPlayerConfig);
@@ -27,3 +30,5 @@ cancelConfigElement.addEventListener('click', closePlayerConfig);
 backdropElement.addEventListener('click', closePlayerConfig);
 
 formElement.addEventListener('submit', setPlayerConfig);
+
+startNewGameElement.addEventListener('click', startNewGame);
